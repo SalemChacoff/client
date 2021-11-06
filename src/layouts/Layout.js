eaa03a45fb626d6e212b91f94cc44748f23c4be7
Salem Chacoff <salem.chacoff.basaure@gmail.com>
2021-11-06 15:10:56 -0300

    Proyect V1.2

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,15 +1,15 @@
-import Navigation from "../components/Navigation"
+import Navigation from "../components/Navigation";
 
 //Todos los componentes tienen una propiedad children
 //En este caso sirve para renderizar los componentes entre medio
 const Layout = ({ children }) => {
-    return (
-        <>
-          <Navigation />
-          {children}
-          <h1>Footer</h1>  
-        </>
-    )
-}
+  return (
+    <>
+      <Navigation />
+      {children}
+      <h1>Este es el footer</h1>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
